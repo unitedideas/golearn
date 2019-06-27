1409af2a02f05fd08eb25eaa42ea7108cb36609b
unitedideas <unitedideas@gmail.com>
2019-06-26 19:15:51 -0700

    data types

--- code/src/github.com/unitedideas/simpleapp/Main.go
+++ code/src/github.com/unitedideas/simpleapp/Main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
-	fmt.Println("Hello Go!")
+func main() {
+	var n int
+	fmt.Printf("%v, %T\n", n, n)
+	var m bool
+	fmt.Printf("%v, %T\n", m, m)
 }
+
+// var i int = 42
+
+// var (
+// // actorName string = 	'Liz Sladen'
+// // companion string = ' Sarah Smith'
+// // docNum int = 3
+// // season int = 2
+// )
+
+// func main() {
+// 	var i int
+// 	i = 42
+// 	fmt.Printf("%v, %T\n", i, i)
+
+// 	// var j float32 = 27
+// 	// k :=99
+// 	var j string
+// 	j = strconv.Itoa(i)
+// 	fmt.Printf("%v, %T\n", j, j)
+// }
